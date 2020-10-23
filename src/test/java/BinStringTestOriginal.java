d5f2cc1a2d9aeaa1770588172259748b9db26e98
João Cunha <skidrow.jc@gmail.com>
2020-10-24 00:04:54 +0100

    New Test to check random numbers from 0 to 10

--- src/test/java/BinStringTestOriginal.java
+++ src/test/java/BinStringTestOriginal.java
@@ -1,7 +1,9 @@
 import org.junit.jupiter.api.*;
+import java.util.Random;
 
 public class BinStringTestOriginal {
     private BinString binString;
+	private String[] table = { "", "1", "10", "11", "100", "101", "110", "111", "1000", "1001", "1010" }; // binary numbers 0 - 10
 
     @BeforeEach
     public void setUp() {
@@ -27,8 +29,26 @@ public class BinStringTestOriginal {
 
         expected = "11111100";
         Assertions.assertEquals(expected, binString.binarise(252), "252 should be 11111100");
-
     }
+
+	@RepeatedTest(10)
+	public void testBinariseFunctionRandom() {
+		Random r = new Random();
+		int rando = r.nextInt(51); // random number from 0 to 51
+		String bin = binString.binarise(rando);
+
+		for(int i = 0; i < table.length; i++) {
+			if (table[i].equals(bin)) {
+				Assertions.assertEquals(table[i], bin, "Random number from 1 to 10 to binary converter");
+				return;
+			}
+		}
+
+		if (rando > 10)
+			Assertions.assertTrue(rando > 10, "No suport for numbers bigger than 10");
+		else
+			Assertions.fail("The table array sould be able to handle from 0 to 10 numbers");
+	}
 
     @Test
     public void testTotalConversion(){
